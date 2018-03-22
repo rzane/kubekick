18de06383600b582ca93a530c2241674a2502d4f
Ray Zane <ray@promptworks.com>
2018-03-22 15:55:36 -0400

    Decrypt EJSON

---
+++ spec/kubekick/ejson_spec.cr
@@ -0,0 +1,54 @@
+require "minitest/autorun"
+require "../../src/kubekick/ejson"
+
+describe Kubekick::EJSON do
+  include Kubekick
+
+  let :public do
+    "870c8b7867009b93652549c4144cd425bd03043a36e207cce13411d260385732"
+  end
+
+  let :secret do
+    "1d59c0892d42cd5959dd6b8e8b04f1a0061a4c4901e075bad291c109aeac1688"
+  end
+
+  let :box do
+    Crypto.encrypt("foobar", public, secret)
+  end
+
+  let :shallow do
+    %({"example": "#{box}"})
+  end
+
+  let :deep do
+    %({"deep": [{"example": "#{box}"}]})
+  end
+
+  let :array do
+    %([{"example": "#{box}"}])
+  end
+
+  let :ignored do
+    %({"_example": "#{box}"})
+  end
+
+  it "can decrypt shallow properties" do
+    result = EJSON.decrypt(shallow, secret)
+    result["example"].as_s.must_equal("foobar")
+  end
+
+  it "can decrypt deep properties" do
+    result = EJSON.decrypt(deep, secret)
+    result["deep"][0]["example"].as_s.must_equal("foobar")
+  end
+
+  it "can decrypt arrays" do
+    result = EJSON.decrypt(array, secret)
+    result[0]["example"].as_s.must_equal("foobar")
+  end
+
+  it "ignores underscores" do
+    result = EJSON.decrypt(ignored, secret)
+    result["_example"].as_s.must_equal(box)
+  end
+end
---
+++ src/kubekick/ejson.cr
@@ -0,0 +1,43 @@
+require "json"
+require "./crypto"
+
+module Kubekick
+  module EJSON
+    class StructureError < Exception
+    end
+
+    def self.decrypt(data, secret)
+      encrypted = JSON.parse_raw(data)
+      decrypted = walk(encrypted, secret)
+      JSON::Any.new(decrypted)
+    end
+
+    def self.walk(data : Hash(String, JSON::Type), secret)
+      result = {} of String => JSON::Type
+
+      data.each do |key, value|
+        if value.is_a?(String) && !key.starts_with?("_")
+          result[key] = Crypto.decrypt(value, secret)
+        else
+          result[key] = walk(value, secret)
+        end
+      end
+
+      result
+    end
+
+    def self.walk(data : Array(JSON::Type), secret)
+      result = [] of JSON::Type
+
+      data.each do |item|
+        result << walk(item, secret)
+      end
+
+      result
+    end
+
+    def self.walk(data, _secret)
+      data
+    end
+  end
+end
