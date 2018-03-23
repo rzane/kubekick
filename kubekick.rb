21611442cc904a6cfd709ef0ff5d97943fd3c9a5
Ray Zane <ray@promptworks.com>
2018-03-23 00:20:53 -0400

    Fix homebrew formula

--- kubekick.rb
+++ kubekick.rb
@@ -2,7 +2,7 @@ class Kubekick < Formula
   desc "One-off tasks and encrypted secrets CLI for Kubernetes"
   homepage "https://gitlab.com/rzane/kubekick"
   url "https://gitlab.com/rzane/kubekick/repository/v0.1.0/archive.tar.gz"
-  sha256 "aa04f2e39150923e8380bf498f6b191a6c03823faa60d099537937a70ee3717b"
+  sha256 "ed33da4366c64fee548b3f32f1217294ad617004578ddd5d843a8a5c3cefde2e"
   head "https://gitlab.com/rzane/kubekick.git"
 
   depends_on "crystal-lang" => :build
