19967f79462399e64f4e2d113bae2f0d6240a0ca
Ray Zane <ray@promptworks.com>
2018-03-23 16:20:14 -0400

    Try that again...

---
+++ travis-install-sodium.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+# check if libsodium is already installed
+if [ ! -d "$HOME/libsodium/lib" ]; then
+  wget https://github.com/jedisct1/libsodium/releases/download/1.0.11/libsodium-1.0.11.tar.gz
+  tar xvfz libsodium-1.0.11.tar.gz
+  cd libsodium-1.0.11
+  ./configure --prefix=$HOME/libsodium
+  make
+  make install
+else
+  echo 'Using cached directory.'
+fi
