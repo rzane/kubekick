bde78f6fdba8625c74af11924fd362d3e4086534
Ray Zane <ray@promptworks.com>
2018-03-23 10:27:04 -0400

    Update SHA

--- kubekick.rb
+++ kubekick.rb
@@ -2,10 +2,10 @@ class Kubekick < Formula
   desc "One-off tasks and encrypted secrets CLI for Kubernetes"
   homepage "https://gitlab.com/rzane/kubekick"
   url "https://gitlab.com/rzane/kubekick/repository/v0.1.0/archive.tar.gz"
-  sha256 "ed33da4366c64fee548b3f32f1217294ad617004578ddd5d843a8a5c3cefde2e"
+  sha256 "51343e7a776c3764fc5c2e1595739f823d683f36c13fb999344c2728d164b22e"
   head "https://gitlab.com/rzane/kubekick.git"
 
-  depends_on "crystal-lang" #=> :build
+  depends_on "crystal-lang" => :build
   depends_on "libsodium"
 
   def install
